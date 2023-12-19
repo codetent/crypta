2a4980ba1daf9440da3e9ec370ca39be5284c596
Dominik Hauk <dominik.hauk@elektrobit.com>
2023-12-19 12:19:55 +0000

    GH-12: add daemon start command

--- cmd/daemon.go
+++ cmd/daemon.go
@@ -1,12 +1,17 @@
 package cmd
 
 import (
+	"os"
+	"os/exec"
+
 	"github.com/codetent/crypta/pkg/daemon"
 	"github.com/spf13/cobra"
 )
 
 type daemonCmd struct {
 	global *globalFlags
+
+	detached bool
 }
 
 func NewDaemonCmd(global *globalFlags) *cobra.Command {
@@ -15,14 +20,48 @@ func NewDaemonCmd(global *globalFlags) *cobra.Command {
 		Use:   "daemon",
 		Short: "Run the crypta daemon",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return c.Run(args)
+			if !c.detached {
+				return cmd.Help()
+			}
+
+			return c.run()
 		},
 	}
+
+	start := &cobra.Command{
+		Use:   "start",
+		Short: "Starts the crypta daemon",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return c.start(args)
+		},
+	}
+
+	cc.AddCommand(start)
+
+	cc.Flags().BoolVar(&c.detached, "detached", false, "Runs the daemon detached")
+	_ = cc.Flags().MarkHidden("detached")
 
 	return cc
 }
 
-func (c *daemonCmd) Run(args []string) error {
+func (c *daemonCmd) start(args []string) error {
+	cwd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	ex, err := os.Executable()
+	if err != nil {
+		return err
+	}
+
+	// FIXME: Pass global flags to command, in order to be able to set the endpoint
+	args = append(args, "daemon", "--detached")
+	cmd := exec.Command(ex, args...)
+	cmd.Dir = cwd
+	return cmd.Start()
+}
+
+func (c *daemonCmd) run() error {
 	server := daemon.NewDaemonServer(c.global.ip, c.global.port)
 	return server.ListenAndServe()
 }
