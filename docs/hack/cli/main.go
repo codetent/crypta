51c68a61ccabfe0e9e891a968137eb13e07fbdb8
codetent <22278777+codetent@users.noreply.github.com>
2023-12-19 00:59:31 +0000

    GH-15: remove underline in command

--- docs/hack/cli/main.go
+++ docs/hack/cli/main.go
@@ -1,13 +1,32 @@
 package main
 
 import (
+	"path/filepath"
+	"strings"
+
 	"github.com/codetent/crypta/cmd"
 	"github.com/spf13/cobra/doc"
 )
 
+func filePrepender(s string) string {
+	base := strings.TrimSuffix(filepath.Base(s), filepath.Ext(s))
+	cmdline := strings.Join(strings.Split(base, "_"), " ")
+	pre := []string{
+		"---",
+		"hide_title: true",
+		"sidebar_label: " + cmdline,
+		"---",
+	}
+	return strings.Join(pre, "\n") + "\n"
+}
+
+func linkHandler(s string) string {
+	return s
+}
+
 func main() {
 	cmd := cmd.NewRootCmd()
-	err := doc.GenMarkdownTree(cmd, "docs/pages/cli")
+	err := doc.GenMarkdownTreeCustom(cmd, "docs/pages/cli", filePrepender, linkHandler)
 	if err != nil {
 		panic(err)
 	}
